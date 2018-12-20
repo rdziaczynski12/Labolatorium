519a05551ef6e0582cb571928d1d72ee643dc104
rdziaczynski12 <rdziaczynski@gmail.com>
2018-12-20 11:11:48 +0100

    add project

---
+++ Pracownia_D109/D109/Controllers/SectionController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace D109.Controllers
+{
+    public class SectionController : Controller
+    {
+        public IActionResult Mechatronic()
+        {
+            return View();
+        }
+        public IActionResult Mechatronic1()
+        {
+            return View("Views/Section/Mechatronic/Position1.cshtml");
+        }
+        public IActionResult Mechatronic2()
+        {
+            return View("Views/Section/Mechatronic/Position2.cshtml");
+        }
+        public IActionResult Mechatronic3()
+        {
+            return View("Views/Section/Mechatronic/Position3.cshtml");
+        }
+        public IActionResult Mechatronic4()
+        {
+            return View("Views/Section/Mechatronic/Position4.cshtml");
+        }
+        public IActionResult Mechatronic5()
+        {
+            return View("Views/Section/Mechatronic/Position5.cshtml");
+        }
+        public IActionResult Automation()
+        {
+            return View();
+        }
+        public IActionResult Automation1()
+        {
+            return View("Views/Section/Automation/Position1.cshtml");
+        }
+        public IActionResult Automation2()
+        {
+            return View("Views/Section/Automation/Position2.cshtml");
+        }
+        public IActionResult Automation3()
+        {
+            return View("Views/Section/Automation/Position3.cshtml");
+        }
+        public IActionResult Automation4()
+        {
+            return View("Views/Section/Automation/Position4.cshtml");
+        }
+        public IActionResult Automation5()
+        {
+            return View("Views/Section/Automation/Position5.cshtml");
+        }
+        public IActionResult Industry()
+        {
+            return View();
+        }
+        public IActionResult Room()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ Pracownia_D109/D109/wwwroot/js/galery.js
@@ -0,0 +1,31 @@
+﻿var bigImage = document.querySelector('.bigImage > img');
+
+var miniImg = document.querySelector('.miniImg');
+
+miniImg.onclick = function (e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement;
+
+    while (target != this) {
+
+        if (target.nodeName == 'A') {
+            DisplayMini(target.href, target.title);
+            return false;
+        }
+
+        target = target.parentNode;
+    }
+}
+
+function DisplayMini(href, title) {
+    bigImage.src = href;
+    bigImage.alt = title;
+}
+
+var imgs = miniImg.getElementsByTagName('img');
+
+for (var i = 0; i < imgs.length; i++) {
+    var url = imgs[i].parentNode.href;
+    var img = document.createElement('img');
+    img.src = url;
+}
